08e8a784b35ddd5287a541a6b58a08cee63b19bf
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-10-16 00:26:41 -0400

    render license section
    renderLicenseSection function ?complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -38,7 +38,19 @@ function renderLicenseLink(license) {
 }
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+     if (license) {
+          return `
+   ## License
+   
+   This application is licensed under the ${license} license. See the following link for more information: ${renderLicenseLink(
+               license
+          )}
+   `;
+     } else {
+          return "";
+     }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
