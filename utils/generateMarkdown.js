e295ca2cdf0c6ac6f2b8c5ec6847c67cd2003691
RobSprouse <141681347+RobSprouse@users.noreply.github.com>
2023-10-16 00:24:44 -0400

    render license badge and link
    render license badge and link functions ?
    complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,11 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Source: Link to a repository containing Markdown License Badges https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+function renderLicenseBadge(license) {
+     switch (license) {
+          case "MIT":
+               return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+          case "Apache License 2.0":
+               return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+          case "GNU GPL v3":
+               return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+          case "BSD License":
+               return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+          case "Mozilla":
+               return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+          default:
+               return "";
+     }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+     switch (license) {
+          case "MIT":
+               return "https://opensource.org/licenses/MIT";
+          case "Apache License 2.0":
+               return "https://opensource.org/licenses/Apache-2.0";
+          case "GNU GPL v3":
+               return "https://www.gnu.org/licenses/gpl-3.0";
+          case "BSD License":
+               return "https://opensource.org/licenses/BSD-3-Clause";
+          case "Mozilla":
+               return "https://opensource.org/licenses/MPL-2.0";
+          default:
+               return "";
+     }
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
